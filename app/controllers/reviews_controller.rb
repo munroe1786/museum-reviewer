f1bc19dfd2af0e17018ceed7664cd39a64f5e39d
munroe1786 <era3munroe@gmail.com>
2019-11-08 14:31:23 -0500

    correcting typo in get '/reviews/new / building out post '/reviews route / revising museum index to only list all museums for now / building out museum new.html.erb / adding logic to reviews index.html.erb / adding code to reviews new.html.erb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,18 @@ class ReviewsController < ApplicationController
     end
 
     get '/reviews/new' do
-      @musuems = Museum.all
+      @museums = Museum.all
       erb :'/reviews/new.html'
     end
 
-    post '/reviews/new' do
+    post '/reviews' do
+      @review = Review.create(params["review"])
+      @review.museum = Museum.create(name: params["museum"]["name"]["location"])
+      @review.save
+      redirect to "reviews/#{@review.id}"
+    end
 
+    get '/reviews/:id' do
+      erb :'/reviews/show.html'
     end
 end
