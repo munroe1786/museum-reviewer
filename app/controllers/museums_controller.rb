f7df1b98f2459738a1f984c21fc0f41644ffd0c3
munroe1786 <era3munroe@gmail.com>
2019-11-08 07:07:19 -0500

    adding a post '/museums' route / adding a get 'museums/new' route (need to edit) / adding get and post '/reviews/new' routes / revising museums index form to flow better and adding 'Write a Review' button / creating museums and reviews 'new' views

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,10 +1,13 @@
 class MuseumsController < ApplicationController
     get '/museums' do
-        @museum = Museum.all 
+        @museums = Museum.all 
         erb :'/museums/index.html'
     end
 
     post '/museums' do
-        
+        erb :'/museums/new.html'
+    end
+
+    get 'museums/new' do
     end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,15 @@
 class ReviewsController < ApplicationController
     get '/reviews' do
-      @review = Review.all 
+      @reviews = Review.all 
       erb :'/reviews/index.html'
+    end
+
+    get '/reviews/new' do
+      @musuems = Museum.all
+      erb :'/reviews/new.html'
+    end
+
+    post '/reviews/new' do
+
     end
 end
